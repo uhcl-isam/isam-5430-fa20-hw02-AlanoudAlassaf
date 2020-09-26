a73488ef546014fc1b898fcdbbb40281e5197356
Alanoud Alassaf <alassafa2180@uhcl.edu>
2020-09-26 18:01:30 +0300

    HW02

--- CSharp.Assignments.Selection1/Logic1.cs
+++ CSharp.Assignments.Selection1/Logic1.cs
@@ -226,7 +226,7 @@ namespace CSharp.Assignments.Loop1
         /// <param name="number">Number.</param>
         public static bool IsHilly(int number)
         {
-            throw new NotImplementedException();
+            throw new NotImplementedException();//
         }
 
         /// <summary>
